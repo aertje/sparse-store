a5ee2ef5fea13e13282af24903370c9c9daca5f5
Aert van de Hulsbeek <vandehulsbeek@gmail.com>
2023-11-22 01:04:38 +1100

    Initial commit

---
+++ store/store.go
@@ -0,0 +1,189 @@
+package store
+
+import (
+	"sort"
+
+	"slices"
+)
+
+const defaultMinContiguous = 16 << 10 // 16 KiB
+
+type entry[T any] struct {
+	order  int
+	offset int
+	data   []T
+}
+
+type entries[T any] []entry[T]
+
+func (e entries[T]) Search(x int) int {
+	return sort.Search(len(e), func(i int) bool {
+		return e[i].offset >= x
+	})
+}
+
+type Store[T any] struct {
+	minContiguous int
+
+	entries     entries[T]
+	insertCount int
+	occupancy   int
+	length      int
+}
+
+type Option[T any] func(*Store[T])
+
+func WithMinContiguous[T any](minContiguous int) Option[T] {
+	return func(c *Store[T]) {
+		c.minContiguous = minContiguous
+	}
+}
+
+func NewStore[T any](opts ...Option[T]) *Store[T] {
+	cache := &Store[T]{
+		minContiguous: defaultMinContiguous,
+	}
+
+	for _, opt := range opts {
+		opt(cache)
+	}
+
+	return cache
+}
+
+func (c *Store[T]) Occupancy() int {
+	return c.occupancy
+}
+
+func (c *Store[T]) Length() int {
+	return c.length
+}
+
+// Has returns true if the cache contains data at `offset` with length
+// `length`.
+func (c *Store[T]) Has(offset int, length int) bool {
+	if len(c.entries) == 0 && length > 0 {
+		return false
+	}
+
+	lastOffset := offset
+	for _, entry := range c.entries {
+		if entry.offset+len(entry.data) < offset {
+			continue
+		}
+		if entry.offset > offset+length {
+			break
+		}
+
+		if lastOffset < entry.offset {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Get populates `p` with the data at `offset`. If the cache does not contain the
+// complete data for this range, Get returns false.
+func (c *Store[T]) Get(offset int, p []T) bool {
+	if len(c.entries) == 0 && len(p) > 0 {
+		return false
+	}
+
+	lastOffset := offset
+	complete := true
+	for _, entry := range c.entries {
+		if entry.offset+len(entry.data) < offset {
+			continue
+		}
+		if entry.offset > offset+len(p) {
+			break
+		}
+
+		if lastOffset < entry.offset {
+			complete = false
+		}
+
+		offsetDelta := entry.offset - offset
+		if offsetDelta < 0 {
+			copy(p, entry.data[-offsetDelta:])
+		} else {
+			copy(p[offsetDelta:], entry.data)
+		}
+
+		lastOffset = entry.offset + len(entry.data)
+	}
+
+	return complete
+}
+
+// Set sets the cache data at `offset` to `p`. If the cache already contains
+// data at `offset`, it is overwritten.
+func (c *Store[T]) Set(offset int, p []T) {
+	i := c.entries.Search(offset)
+	c.entries = slices.Insert(c.entries, i, entry[T]{c.insertCount, offset, p})
+	c.insertCount++
+
+	// If the length increased, update it.
+	if c.length < offset+len(p) {
+		c.length = offset + len(p)
+	}
+
+	// Update the occupancy optimistically. If the entry is compacted, the
+	// occupancy will be updated again.
+	c.occupancy += len(p)
+
+	c.compact()
+}
+
+// compact compacts the cache by merging adjacent entries and removing
+// overlapping entries.
+func (c *Store[T]) compact() {
+	for i := 0; i < len(c.entries)-1; i++ {
+		// We use references here as we want to update the entries in place
+		// when reslicing.
+		current := &c.entries[i]
+		next := &c.entries[i+1]
+
+		currentMin := current.offset
+		currentMax := current.offset + len(current.data)
+		nextMin := next.offset
+		nextMax := next.offset + len(next.data)
+
+		if nextMin < currentMax {
+			// If the current entry encompasses the next entry, copy if needed.
+			if nextMax <= currentMax {
+				// If the next entry has a higher order, copy.
+				if current.order < next.order {
+					copy(current.data[nextMin-currentMin:], next.data)
+				}
+
+				c.entries = append(c.entries[:i+1], c.entries[i+2:]...)
+				c.occupancy -= len(next.data)
+				i--
+				continue
+			} else {
+				// If the entries overlap reslice so that they become contiguous.
+				c.occupancy -= currentMax - nextMin
+				if current.order < next.order {
+					current.data = current.data[:nextMin-currentMin]
+					currentMax = nextMin
+				} else {
+					next.data = next.data[currentMax-nextMin:]
+					next.offset = currentMax
+					nextMin = currentMax
+				}
+			}
+		}
+
+		// If the entries are contiguous and small enough, combine them.
+		if currentMax == nextMin && nextMax-currentMin <= c.minContiguous {
+			newData := make([]T, nextMax-currentMin)
+			copy(newData, current.data)
+			copy(newData[currentMax-currentMin:], next.data)
+			c.entries[i] = entry[T]{current.order, currentMin, newData}
+			c.entries = append(c.entries[:i+1], c.entries[i+2:]...)
+			i--
+		}
+	}
+}
---
+++ store/store_test.go
@@ -0,0 +1,318 @@
+package store_test
+
+import (
+	"fmt"
+	"math/rand"
+
+	"testing"
+
+	"github.com/aertje/sparse-store/store"
+	"github.com/stretchr/testify/assert"
+)
+
+type entry struct {
+	offset int
+	data   []byte
+}
+
+func TestStoreSet(t *testing.T) {
+	for _, topt := range []struct {
+		name string
+		opt  store.Option[byte]
+	}{
+		{
+			name: "default",
+			opt:  store.WithMinContiguous[byte](16 << 10), // 16 KiB
+		},
+		{
+			name: "never merge",
+			opt:  store.WithMinContiguous[byte](1),
+		},
+	} {
+		{
+			for _, tc := range []struct {
+				name              string
+				content           []entry
+				expectedLength    int
+				expectedOccupancy int
+				expectedContent   []byte
+			}{
+				{
+					name:              "empty",
+					content:           []entry{},
+					expectedLength:    0,
+					expectedOccupancy: 0,
+					expectedContent:   []byte{0, 0},
+				},
+				{
+					name: "continuous",
+					content: []entry{
+						{offset: 0, data: []byte{0}},
+						{offset: 1, data: []byte{1}},
+					},
+					expectedLength:    2,
+					expectedOccupancy: 2,
+					expectedContent:   []byte{0, 1},
+				},
+				{
+					name: "continuous other order",
+					content: []entry{
+						{offset: 1, data: []byte{1}},
+						{offset: 0, data: []byte{0}},
+					},
+					expectedLength:    2,
+					expectedOccupancy: 2,
+					expectedContent:   []byte{0, 1},
+				},
+				{
+					name: "overlapping",
+					content: []entry{
+						{offset: 0, data: []byte{0, 1}},
+						{offset: 1, data: []byte{10, 2}},
+					},
+					expectedLength:    3,
+					expectedOccupancy: 3,
+					expectedContent:   []byte{0, 10, 2},
+				},
+				{
+					name: "overlapping other order",
+					content: []entry{
+						{offset: 1, data: []byte{1, 2}},
+						{offset: 0, data: []byte{0, 10}},
+					},
+					expectedLength:    3,
+					expectedOccupancy: 3,
+					expectedContent:   []byte{0, 10, 2},
+				},
+				{
+					name: "inside",
+					content: []entry{
+						{offset: 0, data: []byte{0, 1, 2}},
+						{offset: 1, data: []byte{10}},
+					},
+					expectedLength:    3,
+					expectedOccupancy: 3,
+					expectedContent:   []byte{0, 10, 2},
+				},
+				{
+					name: "gap",
+					content: []entry{
+						{offset: 0, data: []byte{0, 1, 2}},
+						{offset: 4, data: []byte{4}},
+					},
+					expectedLength:    5,
+					expectedOccupancy: 4,
+					expectedContent:   []byte{0, 1, 2, 0, 4},
+				},
+				{
+					name: "gap other order",
+					content: []entry{
+						{offset: 4, data: []byte{4}},
+						{offset: 0, data: []byte{0, 1, 2}},
+					},
+					expectedLength:    5,
+					expectedOccupancy: 4,
+					expectedContent:   []byte{0, 1, 2, 0, 4},
+				},
+				{
+					name: "filled gap",
+					content: []entry{
+						{offset: 0, data: []byte{0, 1, 2}},
+						{offset: 4, data: []byte{4}},
+						{offset: 3, data: []byte{3}},
+					},
+					expectedLength:    5,
+					expectedOccupancy: 5,
+					expectedContent:   []byte{0, 1, 2, 3, 4},
+				},
+				{
+					name: "filled gap other order",
+					content: []entry{
+						{offset: 3, data: []byte{3}},
+						{offset: 0, data: []byte{0, 1, 2}},
+						{offset: 4, data: []byte{4}},
+					},
+					expectedLength:    5,
+					expectedOccupancy: 5,
+					expectedContent:   []byte{0, 1, 2, 3, 4},
+				},
+				{
+					name: "double gap encompassed",
+					content: []entry{
+						{offset: 1, data: []byte{1}},
+						{offset: 3, data: []byte{3}},
+						{offset: 0, data: []byte{0, 10, 2, 30, 4}},
+					},
+					expectedLength:    5,
+					expectedOccupancy: 5,
+					expectedContent:   []byte{0, 10, 2, 30, 4},
+				},
+				{
+					name: "double gap filled",
+					content: []entry{
+						{offset: 1, data: []byte{1}},
+						{offset: 3, data: []byte{3}},
+						{offset: 1, data: []byte{10, 2, 30}},
+					},
+					expectedLength:    4,
+					expectedOccupancy: 3,
+					expectedContent:   []byte{0, 10, 2, 30},
+				},
+				{
+					name: "double gap not filled",
+					content: []entry{
+						{offset: 1, data: []byte{1}},
+						{offset: 3, data: []byte{3}},
+						{offset: 3, data: []byte{30, 4}},
+					},
+					expectedLength:    5,
+					expectedOccupancy: 3,
+					expectedContent:   []byte{0, 1, 0, 30, 4},
+				},
+			} {
+				t.Run(fmt.Sprintf("%v %v", topt.name, tc.name), func(t *testing.T) {
+					s := store.NewStore(topt.opt)
+
+					for _, entry := range tc.content {
+						s.Set(entry.offset, entry.data)
+					}
+
+					assert.Equal(t, tc.expectedLength, s.Length())
+					assert.Equal(t, tc.expectedOccupancy, s.Occupancy())
+					data := make([]byte, len(tc.expectedContent))
+					s.Get(0, data)
+					assert.Equal(t, tc.expectedContent, data)
+				})
+			}
+		}
+	}
+}
+
+func TestStoreGetAndHas(t *testing.T) {
+	for _, tc := range []struct {
+		name            string
+		content         []entry
+		offset          int
+		expectedContent []byte
+		expectHas       bool
+	}{
+		{
+			name:            "empty, nothing requested",
+			content:         []entry{},
+			offset:          0,
+			expectedContent: []byte{},
+			expectHas:       true,
+		},
+		{
+			name:            "empty with overfetch",
+			content:         []entry{},
+			offset:          1,
+			expectedContent: []byte{0, 0},
+			expectHas:       false,
+		},
+		{
+			name: "continuous",
+			content: []entry{
+				{offset: 1, data: []byte{1, 2}},
+			},
+			offset:          1,
+			expectedContent: []byte{1, 2},
+			expectHas:       true,
+		},
+		{
+			name: "continuous with inside fetch",
+			content: []entry{
+				{offset: 1, data: []byte{1, 2, 3}},
+			},
+			offset:          2,
+			expectedContent: []byte{2, 3},
+			expectHas:       true,
+		},
+		{
+			name: "continuous with underfetch",
+			content: []entry{
+				{offset: 1, data: []byte{1, 2}},
+			},
+			offset:          0,
+			expectedContent: []byte{0, 1, 2},
+			expectHas:       false,
+		},
+		{
+			name: "continuous with under- and overfetch",
+			content: []entry{
+				{offset: 1, data: []byte{1, 2}},
+			},
+			offset:          0,
+			expectedContent: []byte{0, 1, 2, 0},
+			expectHas:       false,
+		},
+		{
+			name: "gap with offset",
+			content: []entry{
+				{offset: 1, data: []byte{1}},
+				{offset: 3, data: []byte{3}},
+			},
+			offset:          1,
+			expectedContent: []byte{1, 0, 3},
+			expectHas:       false,
+		},
+		{
+			name: "gap with fetch from gap",
+			content: []entry{
+				{offset: 1, data: []byte{1}},
+				{offset: 3, data: []byte{3}},
+			},
+			offset:          2,
+			expectedContent: []byte{0, 3},
+			expectHas:       false,
+		},
+		{
+			name: "gap with fetch after gap",
+			content: []entry{
+				{offset: 1, data: []byte{1}},
+				{offset: 3, data: []byte{3}},
+			},
+			offset:          3,
+			expectedContent: []byte{3},
+			expectHas:       true,
+		},
+		{
+			name: "gap with fetch before gap",
+			content: []entry{
+				{offset: 1, data: []byte{1}},
+				{offset: 3, data: []byte{3}},
+			},
+			offset:          1,
+			expectedContent: []byte{1},
+			expectHas:       true,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			s := store.NewStore[byte]()
+
+			for _, entry := range tc.content {
+				s.Set(entry.offset, entry.data)
+			}
+
+			data := make([]byte, len(tc.expectedContent))
+			complete := s.Get(tc.offset, data)
+			assert.Equal(t, tc.expectedContent, data)
+			assert.Equal(t, tc.expectHas, complete)
+
+			has := s.Has(tc.offset, len(tc.expectedContent))
+			assert.Equal(t, tc.expectHas, has)
+		})
+	}
+}
+
+func BenchmarkStoreSet(b *testing.B) {
+	s := store.NewStore[byte]()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		buf := make([]byte, rand.Intn(1<<20)) // 1MiB
+		b.StartTimer()
+		s.Set(rand.Intn(1<<30), buf) // 1GiB
+		b.StopTimer()
+	}
+}
